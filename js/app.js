cf6fdf18441c0d626b09855b78c7f7bff209ea3b
harryphelps2 <harryphelps1@gmail.com>
2018-12-04 16:16:32 +0000

    Sort out routing for app.

--- js/app.js
+++ js/app.js
@@ -3,24 +3,24 @@ angular.module('SohoStriders', ['ngRoute', 'RouteControllers', 'angular-storage'
 angular.module('SohoStriders').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  
  
-    $routeProvider.when('milestoneproject1/', {
-        templateUrl: 'milestoneproject1/templates/home.html',
+    $routeProvider.when('/milestoneproject1/', {
+        templateUrl: '/milestoneproject1/templates/home.html',
         controller: 'HomeController'
     })
-    .when('milestoneproject1/training', {
-        templateUrl: 'milestoneproject1/templates/training.html',
+    .when('/milestoneproject1/training', {
+        templateUrl: '/milestoneproject1/templates/training.html',
         controller: 'TrainingController'
     })
-    .when('milestoneproject1/racing', {
-        templateUrl: 'milestoneproject1/templates/racing.html',
+    .when('/milestoneproject1/racing', {
+        templateUrl: '/milestoneproject1/templates/racing.html',
         controller: 'RacingController'
     })
-    .when('milestoneproject1/schedule', {
-    	templateUrl: 'milestoneproject1/templates/schedule.html',
+    .when('/milestoneproject1/schedule', {
+    	templateUrl: '/milestoneproject1/templates/schedule.html',
 	controller: 'ScheduleController'
     })
-    .when('milestoneproject1/support', {
-        templateUrl: 'milestoneproject1/templates/support.html',
+    .when('/milestoneproject1/support', {
+        templateUrl: '/milestoneproject1/templates/support.html',
         controller: 'SupportController'
     });
 });
