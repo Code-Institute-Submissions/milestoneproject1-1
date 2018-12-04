53bf904442fcad74ef1dc7eaed822087baafe555
harryphelps2 <harryphelps1@gmail.com>
2018-12-04 16:03:55 +0000

    Sort out routing for app.

--- js/app.js
+++ js/app.js
@@ -3,24 +3,24 @@ angular.module('SohoStriders', ['ngRoute', 'RouteControllers', 'angular-storage'
 angular.module('SohoStriders').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  
  
-    $routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
+    $routeProvider.when('milestoneproject1/', {
+        templateUrl: 'milestoneproject1/templates/home.html',
         controller: 'HomeController'
     })
     .when('milestoneproject1/training', {
-        templateUrl: 'templates/training.html',
+        templateUrl: 'milestoneproject1/templates/training.html',
         controller: 'TrainingController'
     })
     .when('milestoneproject1/racing', {
-        templateUrl: 'templates/racing.html',
+        templateUrl: 'milestoneproject1/templates/racing.html',
         controller: 'RacingController'
     })
     .when('milestoneproject1/schedule', {
-    	templateUrl: 'templates/schedule.html',
+    	templateUrl: 'milestoneproject1/templates/schedule.html',
 	controller: 'ScheduleController'
     })
     .when('milestoneproject1/support', {
-        templateUrl: 'templates/support.html',
+        templateUrl: 'milestoneproject1/templates/support.html',
         controller: 'SupportController'
     });
 });
